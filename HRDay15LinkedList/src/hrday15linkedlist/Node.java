55966e6544cbb219925adb4dc6a1a2d452bf66f3
Fernando1Borges <centraloro@gmail.com>
2016-12-28 11:08:28 -0600

    Add day 15 linked list HR

---
+++ HRDay15LinkedList/src/hrday15linkedlist/Node.java
@@ -0,0 +1,22 @@
+/*
+ * Hacker Rank Day 15 Linked List
+ * Completed 12/28/16
+ */
+package hrday15linkedlist;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author konradgnat
+ */
+public class  Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+
+  
