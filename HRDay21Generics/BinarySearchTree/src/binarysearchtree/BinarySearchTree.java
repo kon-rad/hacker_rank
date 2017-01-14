48669c9755de11586ba17d70dc86269851f0377c
Fernando1Borges <centraloro@gmail.com>
2017-01-14 08:59:48 -0600

    Add updates to hrday23

---
+++ HRDay21Generics/BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,27 @@
+/*
+ *  Konrad Gnat
+ *  011317
+ *  HackerRank Day 22 Binary Search Tree
+ *  Breath first search: A is root
+ * finds out what is directly connected 
+ * to A
+ * Depth first search is the same idea
+ *  talked about Heaps, min-Heaps
+ * max-heap, binary search trees, 
+ * path-finding algorithms
+ * 
+ */
+package binarysearchtree;
+
+// a tree interface implemented by 
+// 1. a nonempty BST
+// 2. Empty BST
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
