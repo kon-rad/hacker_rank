c04161338dec154fbf8bc0ac75a0cc36b5f4c234
Fernando1Borges <centraloro@gmail.com>
2017-01-20 18:58:43 -0600

    Add day 22

---
+++ HRDay22BinarySearchTree/Tree.java
@@ -0,0 +1,17 @@
+/*
+ *  Konrad Gnat
+ *  
+ * 
+ */
+package binarysearchtree;
+
+/**
+ *
+ * @author konradgnat
+ */
+public interface Tree<D extends Comparable>{
+    public boolean isEmpty();
+    public int cardinality();
+    public boolean member(D elt);
+    public NonEmptyBST<D> add(D elt);
+}
