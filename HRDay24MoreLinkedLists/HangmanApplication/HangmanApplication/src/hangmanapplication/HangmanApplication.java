c9109f0bd2be731e7452c59140b2399e598845ef
Fernando1Borges <centraloro@gmail.com>
2017-02-09 15:32:44 -0600

    Add hangman app progress

---
+++ HRDay24MoreLinkedLists/HangmanApplication/HangmanApplication/src/hangmanapplication/HangmanApplication.java
@@ -0,0 +1,21 @@
+/*
+ *  Konrad Gnat
+ *  Completed 02/09/17
+ * 
+ */
+package hangmanapplication;
+
+import java.util.Scanner;
+
+public class HangmanApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to hangman! I will pick a word and you will"
+                    + "try to guess it. ")
+    }
+    
+}
