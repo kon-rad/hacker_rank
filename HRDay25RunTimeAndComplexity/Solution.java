680e1ef526b9413498af9cdcff299d151a8646bd
konradgnat <konradmgnat@gmail.com>
2017-02-22 20:32:14 -0600

    Add progress to run time and complexity day 25

--- HRDay25RunTimeAndComplexity/Solution.java
+++ HRDay25RunTimeAndComplexity/Solution.java
@@ -19,21 +19,33 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int all[] = new int[n];
-        String result[] = new String[n];
-        
-        for (int i = 0; i < n; i++) {
+        int n = in.nextInt(), i = 0;
+        int all[] = new int[n]; 
+        String result[] = new String[n]; 
+
+        for (i = 0; i < n; i++) {
+            boolean prime = true;
             all[i] = in.nextInt();
-            for(int j = 2; j < all[i]; j++) {
-                if(all[i] % j == 0) {
-                    System.out.println("Not prime");
-                    break;
+            int top = (int)Math.sqrt(all[i]) + 1;
+            
+            if(all[i] > 2 && all[i] % 2 == 0) {
+                System.out.println("Not prime");
+            } else if ( all[0] == 1 && all.length == 1) {
+                System.out.println("Not prime");
+            } else if ((all[i] % 3 == 0 || all[i] % 5 == 0 || all[i] % 7 == 0 || all[i] % 11 == 0) && (all[i] != 5 && all[i] != 7)) {
+                System.out.println("Not prime");
+            } else {
+                for(int j = 3; j < top; j += 2) {
+                    if(all[i] % j == 0) {
+                        System.out.println("Not prime");
+                        prime = false;
+                        break;
+                    }
                 }
-                if(j == (all[i] - 1)) {
-                    System.out.println("Prime");
+                if (prime) {
+                    System.out.println("Prime"); 
                 }
             }
-        }
+        } 
     }
 }
