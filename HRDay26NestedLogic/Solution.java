bdeed6668c3a8f8d9c0730d8d5fc0739a72c4cca
konradgnat <konradmgnat@gmail.com>
2017-05-07 11:26:38 -0500

    Add if statement, set up vim for java use

--- HRDay26NestedLogic/Solution.java
+++ HRDay26NestedLogic/Solution.java
@@ -10,11 +10,12 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         
         Scanner in = new Scanner(System.in);
-        
+	int fine = 0; 
         String returnDate = in.nextLine();   
         String dueDate = in.nextLine();
         String[] returnSplit = returnDate.split(" ");
         int[] returnNums = new int[returnSplit.length];
+
         for (int i = 0; i < returnSplit.length; i++) {
             returnNums[i] = Integer.parseInt(returnSplit[i]);
         }
@@ -29,6 +30,10 @@ public class Solution {
         System.out.println(returnDate);
         System.out.println(dueDate);
         
-        
+        if (dueNums[2] >= returnNums[2] && dueNums[0] >= returnNums[0] && dueNums[1] >= returnNums[1])
+	{
+		fine = 42; 
+	}
+	System.out.println(fine);
     }
 }
